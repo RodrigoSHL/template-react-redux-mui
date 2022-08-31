5c90bed0dd8dacf941cdc26cb99d96ad5fa65124
RodrigoSHL <shl.catalan@gmail.com>
2022-08-31 00:16:07 -0400

    add: integrate snack bar in test component

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,16 +1,46 @@
 import React from "react";
-import { Box, Button, Container, Stack } from "@mui/material";
+import {
+  Alert,
+  AlertProps,
+  Box,
+  Button,
+  Container,
+  Snackbar,
+  Stack,
+} from "@mui/material";
 import styles from "./TestComponent.module.css";
 
 const TestComponent = () => {
+  const [snackbar, setSnackbar] = React.useState<Pick<
+    AlertProps,
+    "children" | "severity"
+  > | null>(null);
+
+  const handleCloseSnackbar = () => setSnackbar(null);
+
+  const openSuccessSnack = () => {
+    setSnackbar({
+      children: "Success Snackbar",
+      severity: "success",
+    });
+  };
+
+  const openErrorSnack = () => {
+    setSnackbar({
+      children: "Error Snackbar",
+      severity: "error",
+    });
+  };
+
   return (
     <>
       <Container maxWidth="lg">
         <Box className={styles.container}>
           <Box component="main" className={styles.containerTable}>
-            <h1>Hola Mundo</h1>
+            <h1>Testing</h1>
             <Stack direction="row" spacing={2}>
-              <Button variant="contained">Contained</Button>
+              <Button variant="contained" onClick={openSuccessSnack}>Success</Button>
+              <Button variant="contained" onClick={openErrorSnack}>Error</Button>
               <Button variant="contained" disabled>
                 Disabled
               </Button>
@@ -20,6 +50,11 @@ const TestComponent = () => {
             </Stack>
           </Box>
         </Box>
+        {!!snackbar && (
+          <Snackbar open onClose={handleCloseSnackbar} autoHideDuration={3000}>
+            <Alert {...snackbar} onClose={handleCloseSnackbar} />
+          </Snackbar>
+        )}
       </Container>
     </>
   );
