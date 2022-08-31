96ab52d87e24bac17352a95fd0a1bca55d60b32a
RodrigoSHL <shl.catalan@gmail.com>
2022-08-31 00:16:53 -0400

    add: new path added

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Counter } from "../components/Counter/Counter";
+import Pokeapi from "../components/Pokeapi/Pokeapi";
 import TestComponent from "../components/TestComponent/TestComponent";
 
 const Navigation = () => {
@@ -9,7 +10,7 @@ const Navigation = () => {
       <Routes>
         <Route path="/counter" element={<Counter />} />
         <Route path="/test" element={<TestComponent />} />
-
+        <Route path="/pokeapi" element={<Pokeapi />} />
       </Routes>
     </>
   );
