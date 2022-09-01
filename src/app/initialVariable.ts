129bb4b2d5e6d2dde2cde98199d030f4dd48b34e
RodrigoSHL <shl.catalan@gmail.com>
2022-09-01 15:32:22 -0400

    feat: add initial state for pokemonSlice

--- src/app/initialVariable.ts
+++ src/app/initialVariable.ts
@@ -1,16 +1,43 @@
 import { AlertColor } from "@mui/material";
 
-const severityColor:AlertColor = 'warning'
+const severityColor: AlertColor = "warning";
+interface IPokemonInfo {
+  name: string;
+  weight: number;
+  height: number;
+  sprites: string;
+  types: string[]
+}
 
-/*Snackbar */
+const Pokemon:IPokemonInfo = {name:'',weight:0,height:0,sprites:'',types:[]}
+
+/* Pokeapi */
+export const initialStateCrud = {
+  result: []
+};
+
+/* Pokeapi */
+export const initialStatePokeapi = {
+  count: 0,
+  next: null,
+  previous: null,
+  results: [],
+  onePokemon: Pokemon,
+};
+
+/* Snackbar */
 export const initialStateSetSnackbar = {
   isOpen: false,
-  message: '',
+  message: "",
   severity: severityColor,
-  timeOut: 0
-}
+  timeOut: 0,
+};
 
 export const initialStateSnack = {
-  dataSnackbar: {...initialStateSetSnackbar}
-}
+  dataSnackbar: { ...initialStateSetSnackbar },
+};
 
+/* Slice Base */
+export const initialStateExample = {
+  listExample: [],
+};
