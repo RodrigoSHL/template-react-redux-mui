32d34a5fbe977145858c35fcb548f22f79d5cc1a
RodrigoSHL <shl.catalan@gmail.com>
2022-09-01 17:30:49 -0400

    feat: add complete example in class

--- src/features/example/exampleSlice.ts
+++ src/features/example/exampleSlice.ts
@@ -1,13 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { initialStatePokeapi } from "../../app/initialVariable";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  initialStateExample,
+} from "../../app/initialVariable";
+import { RootState } from "../../app/store";
 
 //Example slice for redux-toolkit bases
 import { types } from "../../app/types";
 
-export const themeSlice = createSlice({
-  name: types.pokeapiType,
-  initialState: {...initialStatePokeapi},
-  reducers: {},
+export const exampleSlice = createSlice({
+  name: types.exampleType,
+  initialState: { ...initialStateExample },
+  reducers: {
+    getAll: (state: any = initialStateExample, action: PayloadAction<any>) => {
+      return { ...state, results: action.payload };
+    },
+  },
 });
 
-export default themeSlice.reducer;
+// ACTION
+export const { getAll } = exampleSlice.actions;
+
+// BUSINESS
+export const exampleFetch = () => async (dispatch: any, getState: any) => {
+  return true;
+};
+
+// STATE-REDUCER
+export const selectExample = (state: RootState) => state.example;
+
+// REDUCER
+export default exampleSlice.reducer;
