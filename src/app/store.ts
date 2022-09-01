5a4957aad00a701d438270162d0408e0838c9ea0
RodrigoSHL <shl.catalan@gmail.com>
2022-09-01 15:32:53 -0400

    feat: add example, pokeapi and crud to redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,18 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import darkModeReducer from "../features/thematic/darkModeSlice";
 import snackbarReducer from "../features/snackbar/snackbarSlice";
+import pokeapiReducer from "../features/pokeapi/pokeapiSlice";
+import exampleReducer from "../features/example/exampleSlice";
+import crudReducer from "../features/crud/crudSlice";
 
 export const store = configureStore({
   reducer: {
+    example: exampleReducer,
     counter: counterReducer,
     darkMode: darkModeReducer,
     snackbar: snackbarReducer,
+    pokeapi: pokeapiReducer,
+    crud: crudReducer,
   },
 });
 
