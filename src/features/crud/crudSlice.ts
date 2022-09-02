b6e963f133e7758b653c39e380a37e3a555955b2
RodrigoSHL <shl.catalan@gmail.com>
2022-09-02 13:11:35 -0400

    feat: add new method "deleted pokemon" from HerokuApp

--- src/features/crud/crudSlice.ts
+++ src/features/crud/crudSlice.ts
@@ -6,34 +6,45 @@ import { types } from "../../app/types";
 
 export const crudSlice = createSlice({
   name: types.crudType,
-  initialState: {...initialStateCrud},
+  initialState: { ...initialStateCrud },
   reducers: {
-    getAll: (state: any = initialStateCrud,action: PayloadAction<any>) => {
-        return {...state, results: action.payload}
+    getAll: (state: any = initialStateCrud, action: PayloadAction<any>) => {
+      return { ...state, results: action.payload };
+    },
+    deleteOne: (state: any = initialStateCrud, action: PayloadAction<any>) => {
+      return { ...state, ...action.payload, pokemon: null };
     },
   },
 });
 
 //Action
-export const { getAll } = crudSlice.actions;
-
+export const { getAll, deleteOne } = crudSlice.actions;
 
 //Business
 export const findAll = () => async (dispatch: any) => {
-    await axios
-      .get(`https://pokedex-api-rest.herokuapp.com/api/v2/pokemon?limit=10`)
-      .then((response) => {
-        dispatch(getAll(response.data));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  await axios
+    .get(`https://pokedex-api-rest.herokuapp.com/api/v2/pokemon?limit=600`)
+    .then((response) => {
+      dispatch(getAll(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
+export const remove = (id:string) => async (dispatch: any) => {
+  await axios
+    .delete(`https://pokedex-api-rest.herokuapp.com/api/v2/pokemon/${id}`)
+    .then((response) => {
+      dispatch(deleteOne(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 //State-Reducer
 export const selectCrud = (state: RootState) => state.crud;
-
 
 // Reducer
 export default crudSlice.reducer;
