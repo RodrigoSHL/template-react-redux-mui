cde2c64c57321b31f89ca8b8aa28dd2d72acc830
RodrigoSHL <shl.catalan@gmail.com>
2022-09-04 19:00:48 -0400

    feat: add create and unpdate method

--- src/features/crud/crudSlice.ts
+++ src/features/crud/crudSlice.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 import { initialStateCrud } from "../../app/initialVariable";
 import { RootState } from "../../app/store";
 import { types } from "../../app/types";
+import { errorColor, successColor } from "../../components/Middleware/Snackbar";
+import { IPokemonCreate } from "../../interfaces/IPokemonCreate.interface";
+import { setOpenSnackbar } from "../snackbar/snackbarSlice";
 
 export const crudSlice = createSlice({
   name: types.crudType,
@@ -37,6 +40,30 @@ export const remove = (id:string) => async (dispatch: any) => {
     .delete(`https://pokedex-api-rest.herokuapp.com/api/v2/pokemon/${id}`)
     .then((response) => {
       dispatch(deleteOne(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const create = (obj:IPokemonCreate) => async (dispatch: any) => {
+  await axios
+    .post(`https://pokedex-api-rest.herokuapp.com/api/v2/pokemon/`, obj)
+    .then((response) => {
+      console.log(response)
+      const objSetting = {isOpen: true,message: 'Pokemon creado correctamente',severity: successColor,timeOut : 2000}
+      dispatch(setOpenSnackbar(objSetting)); 
+    })
+    .catch((error) => {
+      const objSetting = {isOpen: true,message: error.message,severity: errorColor,timeOut : 2000}
+      dispatch(setOpenSnackbar(objSetting));    });
+};
+
+export const update = (obj:IPokemonCreate, id:string) => async (dispatch: any) => {
+  await axios
+    .post(`https://pokedex-api-rest.herokuapp.com/api/v2/pokemon/`, obj)
+    .then((response) => {
+      console.log(response)
     })
     .catch((error) => {
       console.log(error);
