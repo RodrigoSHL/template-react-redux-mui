2e0ce8622addff9d382a3c9aa55809ab2ca3cd9f
RodrigoSHL <shl.catalan@gmail.com>
2022-09-11 18:24:23 -0300

    feat: create login component

---
+++ src/components/Auth/Login.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { IUserLogin } from "../../interfaces/IUserLogin.interface";
+import { useAuthStore } from "../../hooks/useAuthStore";
+import { useAppDispatch } from "../../app/hooks";
+import { errorColor } from "../Middleware/Snackbar";
+import { setOpenSnackbar } from "../../features/snackbar/snackbarSlice";
+const objError = {isOpen: true,message: 'Incorrect credentials',severity: errorColor,timeOut : 2000}
+
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://atomdev.cl/">
+        AtomDev
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function SignIn() {
+  const { startLogin, errorMessage } = useAuthStore();
+  const dispatch = useAppDispatch();
+  const [userLogin, setUserLogin] = useState<IUserLogin>({
+    email: "",
+    password: "",
+  });
+
+  const addDataInMemory = (e: any) => {
+    const { name, value } = e.target;
+    setUserLogin((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    startLogin(userLogin);
+  };
+
+  useEffect(() => {
+    if(errorMessage !== undefined) {
+      dispatch(setOpenSnackbar(objError))
+    }
+  
+  
+  }, [errorMessage])
+  
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            onChange={addDataInMemory}
+            value={userLogin.email}
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            onChange={addDataInMemory}
+            value={userLogin.password}
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
+  );
+}
