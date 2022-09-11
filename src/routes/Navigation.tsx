3601e558000580e9eee8111dfeaf70b82abaf189
RodrigoSHL <shl.catalan@gmail.com>
2022-09-11 20:43:21 -0300

    feat: add authenticated strategy in routes

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,21 +1,39 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "../components/Auth/Login";
 import { Counter } from "../components/Counter/Counter";
 import Crud from "../components/Crud/Crud";
 import Pokeapi from "../components/Pokeapi/Pokeapi";
 import TestComponent from "../components/TestComponent/TestComponent";
-import Login from "../components/Auth/Login";
+import { useAuthStore } from "../hooks/useAuthStore";
 
 const Navigation = () => {
+  const { status, checkAuthToken } = useAuthStore();
+  // const authStatus = 'not-authenticated'; // 'authenticated'; // 'not-authenticated';
+
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
+
   return (
     <>
       <Routes>
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/test" element={<TestComponent />} />
-        <Route path="/pokeapi" element={<Pokeapi />} />
-        <Route path="/crud" element={<Crud />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<TestComponent />} />
+        {status === "not-authenticated" ? (
+          <>
+            <Route path="/auth/*" element={<Login />} />
+            <Route path="/*" element={<Navigate to="/auth/login" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/counter" element={<Counter />} />
+            <Route path="/test" element={<TestComponent />} />
+            <Route path="/pokeapi" element={<Pokeapi />} />
+            <Route path="/crud" element={<Crud />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<TestComponent />} />
+            {/* <Route path="/*" element={<Navigate to="/" />} /> */}
+          </>
+        )}
       </Routes>
     </>
   );
