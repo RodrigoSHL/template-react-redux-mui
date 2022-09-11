c4bdf3411bb676eaafb2a054061225baccec47b2
RodrigoSHL <shl.catalan@gmail.com>
2022-09-11 20:42:50 -0300

    feat: complete logout function, check status function and export methods

--- src/hooks/useAuthStore.tsx
+++ src/hooks/useAuthStore.tsx
@@ -1,10 +1,17 @@
 import { authApi } from "../api";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { clearErrorMessage, onChecking, onLogin, onLogout } from "../features/auth/authSlice";
+import {
+  clearErrorMessage,
+  onChecking,
+  onLogin,
+  onLogout,
+} from "../features/auth/authSlice";
 import { IUserLogin } from "../interfaces/IUserLogin.interface";
 
 export const useAuthStore = () => {
-  const { status, user, errorMessage } = useAppSelector((state) => state.auth);
+  const { status, user, errorMessage }: any = useAppSelector(
+    (state) => state.auth
+  );
   const dispatch = useAppDispatch();
 
   const startLogin = async ({ email, password }: IUserLogin) => {
@@ -13,14 +20,35 @@ export const useAuthStore = () => {
       const { data } = await authApi.post("auth/login", { email, password });
       localStorage.setItem("token", data.token);
       localStorage.setItem("token-init-date", new Date().getTime().toString());
-      dispatch(onLogin({name: data._doc.name, uid: data._doc._id}));
+      dispatch(onLogin({ name: data._doc.name, uid: data._doc._id }));
     } catch (error) {
-      dispatch(onLogout('Incorrect credentials'));
+      dispatch(onLogout("Incorrect credentials"));
       setTimeout(() => {
-        dispatch(clearErrorMessage)
-      }, 10)
+        dispatch(clearErrorMessage);
+      }, 10);
     }
   };
+
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return dispatch(onLogout("Authentication error"));
+
+    try {
+      const { data } = await authApi.get("auth/check-status");
+      console.log("data", data);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime().toString());
+      dispatch(onLogin({ name: data._doc.name, uid: data._doc._id }));
+    } catch (error) {
+      localStorage.clear();
+      dispatch(onLogout("Unauthorized"));
+    }
+  };
+
+  const startLogout = async () => {
+    localStorage.clear();
+    dispatch(onLogout('Closed'));
+  }
 
   return {
     //* Properties
@@ -29,6 +57,8 @@ export const useAuthStore = () => {
     user,
 
     //* Methods
+    checkAuthToken,
     startLogin,
+    startLogout,
   };
 };
