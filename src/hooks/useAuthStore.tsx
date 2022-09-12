1b1f04ac1c47e753f54123e75a2c3990f622310f
RodrigoSHL <shl.catalan@gmail.com>
2022-09-11 22:28:46 -0300

    feat: add roles to redux user state

--- src/hooks/useAuthStore.tsx
+++ src/hooks/useAuthStore.tsx
@@ -1,12 +1,16 @@
 import { authApi } from "../api";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { errorColor, successColor } from "../components/Middleware/Snackbar";
 import {
   clearErrorMessage,
   onChecking,
   onLogin,
   onLogout,
 } from "../features/auth/authSlice";
+import { setOpenSnackbar } from "../features/snackbar/snackbarSlice";
 import { IUserLogin } from "../interfaces/IUserLogin.interface";
+const objError = {isOpen: true,message: 'Problems with server',severity: errorColor,timeOut : 4000}
+const objSuccess = {isOpen: true,message: 'Bienvenido',severity: successColor,timeOut : 4000}
 
 export const useAuthStore = () => {
   const { status, user, errorMessage }: any = useAppSelector(
@@ -21,8 +25,10 @@ export const useAuthStore = () => {
       localStorage.setItem("token", data.token);
       localStorage.setItem("token-init-date", new Date().getTime().toString());
       dispatch(onLogin({ name: data._doc.name, uid: data._doc._id }));
+      dispatch(setOpenSnackbar(objSuccess))
     } catch (error) {
       dispatch(onLogout("Incorrect credentials"));
+      dispatch(setOpenSnackbar(objError))
       setTimeout(() => {
         dispatch(clearErrorMessage);
       }, 10);
@@ -32,13 +38,12 @@ export const useAuthStore = () => {
   const checkAuthToken = async () => {
     const token = localStorage.getItem("token");
     if (!token) return dispatch(onLogout("Authentication error"));
-
     try {
       const { data } = await authApi.get("auth/check-status");
       console.log("data", data);
       localStorage.setItem("token", data.token);
       localStorage.setItem("token-init-date", new Date().getTime().toString());
-      dispatch(onLogin({ name: data._doc.name, uid: data._doc._id }));
+      dispatch(onLogin({ name: data._doc.name, uid: data._doc._id, roles: data._doc.roles }));
     } catch (error) {
       localStorage.clear();
       dispatch(onLogout("Unauthorized"));
