94105916191df7aad477b6e35ed6bec7fa39ccdd
RodrigoSHL <shl.catalan@gmail.com>
2022-09-23 18:04:53 -0300

    feat: create public calendar page component

--- src/components/CalendarPublic/CalendarPublicPage.tsx
+++ src/components/CalendarPublic/CalendarPublicPage.tsx
@@ -3,19 +3,23 @@ import styles from "./Calendar.module.css";
 import { Calendar } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { localizer, getMessages } from "../../helpers";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CalendarPublicModal from "./CalendarPublicModal";
 import { useUiStore } from "../../hooks/useUiStore";
 import { useCalendarStore } from "../../hooks/useCalendarStore";
+import { useParams } from "react-router-dom";
 
 const today = new Date();
 
 const CalendarPublicPage = () => {
-  const { events, setActiveEvent, saveSelectedDateEvent } = useCalendarStore();
+  const { events, setActiveEvent, startLoadingEventsByUrlId } =
+    useCalendarStore();
   const { isDateModalOpen, openDateModal, closeDateModal } = useUiStore();
   const [lastView, setLastView] = useState<any>(
     localStorage.getItem("lastView") || "week"
   );
+
+  const params = useParams();
 
   const eventStyleGetter = (
     event: any,
@@ -24,7 +28,7 @@ const CalendarPublicPage = () => {
     isSelected: any
   ) => {
     const style = {
-      backgroundColor: event.bgColor,
+      backgroundColor: event.take ? "#F9861A" : "#1976d2",
       borderRadius: "0px",
       opacity: 0.8,
       color: "white",
@@ -34,43 +38,22 @@ const CalendarPublicPage = () => {
 
   const onDoubleClick = (event: any) => {
     console.log({ doubleClick: event });
-    openDateModal();
+    if( !event.take ) openDateModal();
+
   };
 
   const onSelect = (event: any) => {
-    console.log({ click: event });
     setActiveEvent(event);
-    const style = {
-      backgroundColor: "#f50057",
-      borderRadius: "0px",
-      opacity: 0.8,
-      color: "white",
-    };      
-    return { style };
   };
-  
+
   const onViewChanged = (event: any) => {
     localStorage.setItem("lastView", event);
     setLastView(event);
   };
 
-  const selectEvent = async (slotInfo: any) => {
-    const { start, end } = slotInfo;
-    const agendaInfoDragDrop = {
-      _id: new Date().getTime(),
-      title: "Title",
-      notes: "Nota",
-      start: start,
-      end: end,
-      bgColor: "#fafafa",
-      user: {
-        _id: "123",
-        name: "Rodrigo Catalan",
-      },
-    };
-    console.log("agendaInfoDragDrop", agendaInfoDragDrop);
-    await saveSelectedDateEvent(agendaInfoDragDrop);
-  };
+  useEffect(() => {
+    startLoadingEventsByUrlId(params.id);
+  }, []);
 
   return (
     <>
@@ -90,10 +73,6 @@ const CalendarPublicPage = () => {
               onDoubleClickEvent={onDoubleClick}
               onSelectEvent={onSelect}
               onView={onViewChanged}
-              onSelectSlot={(slotInfo) => {
-                console.log(slotInfo);
-                selectEvent(slotInfo);
-              }}
               min={
                 new Date(
                   today.getFullYear(),
