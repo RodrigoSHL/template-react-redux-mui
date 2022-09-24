ed44a01936d9b8936fe44cef5306ddec831b7b19
RodrigoSHL <shl.catalan@gmail.com>
2022-09-24 16:50:11 -0300

    feat: create shared button component

---
+++ src/components/Calendar/FabShareCalendar.tsx
@@ -0,0 +1,43 @@
+import { Fab, Tooltip } from "@mui/material";
+import styles from "./Calendar.module.css";
+import ShareIcon from "@mui/icons-material/Share";
+import { useAuthStore } from "../../hooks/useAuthStore";
+import { useAppDispatch } from "../../app/hooks";
+import { setOpenSnackbar } from "../../features/snackbar/snackbarSlice";
+import { infoColor } from "../Middleware/Snackbar";
+const objSuccess = {isOpen: true, message: 'link copied successfully', severity: infoColor, timeOut : 2000}
+
+const FabShareCalendar = () => {
+
+  const {user} = useAuthStore();
+  const dispatch = useAppDispatch();
+
+  const copyToClipboard = (e: any) => {
+    e.preventDefault();
+    var aux = document.createElement("input");
+    aux.setAttribute("value", `https://www.atomdev.cl/#/calendar/${user.uid}`);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    dispatch(setOpenSnackbar(objSuccess))
+  };
+
+  return (
+    <>
+      <Tooltip title={`https://www.atomdev.cl/#/calendar/${user.uid}`} placement="right-start">
+        <Fab
+          onClick={(e: any) => copyToClipboard(e)}
+          className={styles.delFab}
+          style={{ marginLeft: "10px" }}
+          color="info"
+          aria-label="add"
+        >
+          <ShareIcon />
+        </Fab>
+      </Tooltip>
+    </>
+  );
+};
+
+export default FabShareCalendar;
