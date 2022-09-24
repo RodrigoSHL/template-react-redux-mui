8f41bff53305b21f9f26184330ae37c0f7a87bdf
RodrigoSHL <shl.catalan@gmail.com>
2022-09-24 15:36:51 -0300

    fix: router (browserRouter) is changed for router (hashRouter) because there were problems in production with browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { ThemeProvider } from "@mui/material/styles";
 import { useAppSelector } from "./app/hooks";
 import { darkTheme, lightTheme } from "./components/theme/theme";
 import Home from "./components/Home/Home";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 export default function App() {
   const theme = useAppSelector((state) => state.darkMode);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider theme={theme.darkTheme ? darkTheme : lightTheme}>
         <Home />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
