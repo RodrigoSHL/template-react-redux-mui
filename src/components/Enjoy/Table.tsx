39bd98076b0d41053299f26fe782c158c4b2022f
RodrigoSHL <shl.catalan@gmail.com>
2022-10-28 20:59:02 -0300

    feat: add new component to navigation path

---
+++ src/components/Enjoy/Table.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+const Table = () => {
+  return <div>
+
+    <h1> Sorteo rifa para el 10 de noviembre de 2022, gracias <EmojiEmotionsIcon/></h1> 
+  </div>;
+};
+
+export default Table;
