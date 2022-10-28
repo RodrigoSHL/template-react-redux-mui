5652d25d675d4dc7cfbbbef188dd2d4622d8a2b8
RodrigoSHL <shl.catalan@gmail.com>
2022-10-28 20:58:48 -0300

    feat: add new component

---
+++ src/components/Enjoy/Enjoy.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+import Table from "./Table";
+
+const Enjoy = () => {
+    return (
+      <Box
+        sx={{
+          flexDirection: "row",
+        }}
+      >
+        <Box>
+          <Table />
+        </Box>
+      </Box>
+    );
+  };
+  
+  export default Enjoy;
